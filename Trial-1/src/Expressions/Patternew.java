50a6e47a9ede2316ce3a9b17b11c82578f5ee610
Vatsal <v.one.lmbc@gmail.com>
2019-09-22 22:34:33 +0530

    Commented Pattern program loop test condition

--- Trial-1/src/Expressions/Patternew.java
+++ Trial-1/src/Expressions/Patternew.java
@@ -9,11 +9,10 @@ public class Patternew {
 			for (int k = 1; k<=3 ; k++) {//loop to print X's on left
 				System.out.print("x");
 			}
-			for (int l = 0; l<=k ; l++) {//loop to print X's on right
-				System.out.print(" ");
+			//for (int l = 0; l<=j ; l++) {//loop to print X's on right
+			//	System.out.print(" ");
 			}
 			
 		}
 	}
 
-}
